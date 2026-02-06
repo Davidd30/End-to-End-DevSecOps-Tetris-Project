d8756b5e1d5e74e28af429f5b61f865541ab2e6a
Davidd30 <david.nabil987@gmail.com>
2026-02-07 00:16:35 +0200

    one terraform only

--- Tetris-V2/terraform-project/main.tf
+++ Tetris-V2/terraform-project/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_s3_bucket" "terraform_state" {
   bucket        = "beshoy-eks-state-unique-id" # غير الاسم ده لاسم فريد
   force_destroy = true # عشان لو حبيت تمسحها بعدين بسهولة
