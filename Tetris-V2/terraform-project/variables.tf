e61ebada0f35ff6fc1ebcb7eddce81bc956ebee5
Davidd30 <david.nabil987@gmail.com>
2026-02-07 00:05:38 +0200

    adding vaiable.tf

---
+++ Tetris-V2/terraform-project/variables.tf
@@ -0,0 +1,6 @@
+variable "region" { type = string }
+variable "vpc_cidr" { type = string }
+variable "public_subnet_cidr" { type = list(string) }
+variable "private_subnet_cidr" { type = list(string) }
+variable "availability_zones" { type = list(string) }
+variable "eks_cluster_name" { type = string }
