0d2554fd78c70fa83553d66a365b6c85d0ca0eeb
marina-ishak <marinarizkallahishak@gmail.com>
2026-02-07 16:03:06 +0200

    Updating k8s files

--- Tetris-V2/terraform/backend/main.tf
+++ Tetris-V2/terraform/backend/main.tf
@@ -7,10 +7,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "practice-eks-install-bucket-rina-2026-01"
-    key     = "eks/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+    bucket         = "practice-eks-install-bucket-rina-2026-01"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
   }
 }
 
--- Tetris-V2/terraformbucket/main.tf
+++ Tetris-V2/terraformbucket/main.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-state-rina-123456789012-us-east-1"
 
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
   }
 
   tags = {
@@ -25,7 +25,34 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
+############################
+# DynamoDB for state locking
+############################
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "terraform-state-locks"
+    Purpose     = "Terraform state locking"
+    Environment = "shared"
+  }
+}
+############################
+# Outputs
+############################
 output "s3_bucket_name" {
   value       = aws_s3_bucket.terraform_state.bucket
   description = "Name of the S3 bucket used for Terraform remote state"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "Name of the DynamoDB table used for state locking"
 }
